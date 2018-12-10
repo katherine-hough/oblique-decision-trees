1ad4292afb25c95e2ec3187f2c9982fdfa534737
katherine-hough <32645020+katherine-hough@users.noreply.github.com>
2018-12-09 20:49:24 -0500

    flushed cache for .gitignore purposes

--- run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set dataset=breast-cancer
-set sparse=dense
-
-javac -Xlint:unchecked -d project/target project/src/*.java
-java -cp project/target CVDriver %sparse% data/%dataset%/%dataset%.data data/%dataset%/%dataset%.labels 10 484 GA-ODT
-
-REM java -cp project/target ClassificationDriver %sparse% data/%dataset%/%dataset%.data data/%dataset%/%dataset%.data data/%dataset%/%dataset%.labels predictions.labels GA-ODT
